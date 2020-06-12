9ce741f67bc6c7691a93c76f8a1db244db342a10
Matthew Dawber <mdawber@yozu.co.uk>
2020-06-12 01:35:50 +0100

    ReactTrixEditor converted to hook.

--- src/ReactTrixEditor.js
+++ src/ReactTrixEditor.js
@@ -1,54 +1,44 @@
-import React, { Component } from 'react';
+import React, { useEffect }  from 'react';
 import 'trix';
 import PropTypes from 'prop-types';
 
-class ReactTrixEditor extends Component {
-  static defaultProps = {
-    autofocus: false,
-    input: 'react-trix-editor',
-    placeholder: 'Enter text here...'
-  }
+const ReactTrixEditor  = (props) => {
+  const id = Math.random().toString(36);
 
-  constructor() {
-    super();
-    this.id = Math.random().toString(36);
-    this.updateStateValue = this.updateStateValue.bind(this);
-  }
-
-  componentDidMount() {
-    // Provide editor to callback on initialisation
-    document.getElementById(this.id).addEventListener('trix-initialize', () => {
-      this.editor = document.getElementById(this.id).editor;
-      if (this.props.onEditor) {
-        // Pass the editor & node
-        this.props.onEditor(this.editor, document.getElementById(this.id));
+  const onStart = () => (
+    document.getElementById(id).addEventListener('trix-initialize', () => {
+      const editor = document.getElementById(id).editor;
+      if (props.onEditor) {
+        props.onEditor(editor, document.getElementById(id));
       }
-    });
+      document.getElementById(id).addEventListener('trix-change', e => update(e));
+    }));
+  useEffect(onStart, []);
 
-    document.getElementById(this.id).addEventListener('trix-change', e => this.updateStateValue(e));
-  }
+  const update = (e) => (
+    props.onChange(e.target.value)
+  );
 
-  updateStateValue(e) {
-    const value = e.target.value;
-    this.props.onChange(value);
-  }
+  const { input, initialValue, placeholder, autofocus } = props;
 
-  render() {
-    const { input, initialValue, placeholder, autofocus } = this.props;
+  return (
+    <div>
+      <input id={input} value={initialValue} type="hidden" name="content" />
+      <trix-editor
+        id={id}
+        input={input}
+        placeholder={placeholder}
+        autofocus={autofocus}
+      />
+    </div>
+  );
+};
 
-    return (
-      <div>
-        <input id={input} value={initialValue} type="hidden" name="content" />
-        <trix-editor
-          id={this.id}
-          input={input}
-          placeholder={placeholder}
-          autofocus={autofocus} />
-      </div>
-    );
-  }
-}
-
+ReactTrixEditor.defaultProps = {
+  autofocus: false,
+  input: 'react-trix-editor',
+  placeholder: 'Enter text here...'
+};
 ReactTrixEditor.propTypes = {
   onChange: PropTypes.func.isRequired,
   onEditor: PropTypes.func,
